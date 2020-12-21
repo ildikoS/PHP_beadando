f214cef8e12eb280949bf22c598595c682fee69b
ildikoS <Ildiko-Soltesz@users.noreply.github.com>
2020-12-21 14:08:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+//
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Főoldal</title>
+</head>
+<body>
+
+<header>
+    <div class="container">
+        <h1><span class="highlight">NemKoViD</span> - Nemzeti Koronavírus Depó</h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">Bejelentkezés</a></li>
+                <li><a href="about.html">Regisztráció</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<section>
+    <div class="container">
+        <p>A Nemzeti Koronavírus Depó (NemKoViD - Mondj nemet a koronavírusra!)
+             központi épületében különböző időpontokban oltásokat szervez.</p>
+    </div>
+</section>
+
+<section>
+    <table>
+
+    </table>
+</section>
+
+
+
+<footer>
+    <p>NemKoViD, Copyright &copy; 2020</p>
+</footer>
+
+</body>
+</html>
